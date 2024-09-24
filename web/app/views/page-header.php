126b952b58fbca328e6ba9ad6ab2ead5763aa6ff
zhao zekun <zekun@mail.ustc.edu.cn>
2024-09-24 14:51:26 +0800

    header

--- web/app/views/page-header.php
+++ web/app/views/page-header.php
@@ -225,7 +225,7 @@
 	</head>
 	<body role="document">
 		<div class="container theme-showcase" role="main">
-			<?php if ($ShowPageHeader === 0): ?>
+			<?php if ($ShowPageHeade): ?>
 			<div>
 				<ul class="nav nav-pills float-right" role="tablist">
 				<?php if (Auth::check()): ?>
