48535eba012e6dabfaca2c70572f5615ced124a3
zhao zekun <zekun@mail.ustc.edu.cn>
2024-10-12 09:46:59 +0800

    add contest with key

--- web/app/controllers/add_contest.php
+++ web/app/controllers/add_contest.php
@@ -53,7 +53,7 @@
 		},
 		null
 	);
-	// key（单位：分钟）
+	// key
 	$time_form->addInput(
 		'key', 'text', 'key', 123456,
 		function($str) {
@@ -74,8 +74,8 @@
 		$esc_name = DB::escape($esc_name);
 		
 		// 插入数据库，新增了 end_time 字段
-		DB::query("INSERT INTO contests (name, start_time, last_min, end_time, status) 
-				   VALUES ('$esc_name', '$start_time_str', {$_POST['last_min']}, '$end_time_str', 'unfinished')");
+		DB::query("INSERT INTO contests (name, start_time, last_min, end_time, status, key) 
+				   VALUES ('$esc_name', '$start_time_str', {$_POST['last_min']}, '$end_time_str', 'unfinished', {$_POST['key']})");
 	};
 
 	// 成功后跳转
