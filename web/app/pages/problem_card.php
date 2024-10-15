e0cf3bf5f99442db7eed749e02bcf97ed003c9ef
zhao zekun <zekun@mail.ustc.edu.cn>
2024-10-16 00:16:26 +0800

    答题卡显示作答

--- web/app/pages/problem_card.php
+++ web/app/pages/problem_card.php
@@ -18,7 +18,15 @@
             <?php
             foreach ($problem_list_res[$cnt] as $p) {
                 $pcnt++;
+                $user = $_SESSION['username'];
+                $conid = $_GET['contest_id'];
+                $probid = $p["id"];
+                if(DB::query("select count(*) from hasanswer where username = '$user' and contest_id = '$conid' and _id = '$probid'")){
                 ?>
+                <li class="new_question_id"><a href="/contest/<?= $contest["id"]; ?>/problem/<?= $p["id"]; ?>"><?= $pcnt; ?></a>
+                </li>
+            <?php }
+            else{ ?>
                 <li class="question_id"><a href="/contest/<?= $contest["id"]; ?>/problem/<?= $p["id"]; ?>"><?= $pcnt; ?></a>
                 </li>
             <?php } ?>
