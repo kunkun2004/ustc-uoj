bfa89965556e1d8a0e5898e0874381fffe826abf
zhao zekun <zekun@mail.ustc.edu.cn>
2024-10-16 15:35:48 +0800

    1

--- web/app/pages/prob_choice.php
+++ web/app/pages/prob_choice.php
@@ -117,11 +117,11 @@ function handleUpload($zip_file_name, $content, $tot_size)
     $result['status'] = "Waiting";
     $result_json = json_encode($result);
 
-    if ($is_in_contest) {
+    // if ($is_in_contest) {
         DB::query("insert into submissions (problem_id, contest_id, submit_time, submitter, content, language, tot_size, status, result, is_hidden) values (${problem['id']}, ${contest['id']}, now(), '${myUser['username']}', '$esc_content', '$esc_language', $tot_size, '${result['status']}', '$result_json', 0)");
-    } else {
-        DB::query("insert into submissions (problem_id, submit_time, submitter, content, language, tot_size, status, result, is_hidden) values (${problem['id']}, now(), '${myUser['username']}', '$esc_content', '$esc_language', $tot_size, '${result['status']}', '$result_json', {$problem['is_hidden']})");
-    }
+    // } else {
+    //     DB::query("insert into submissions (problem_id, submit_time, submitter, content, language, tot_size, status, result, is_hidden) values (${problem['id']}, now(), '${myUser['username']}', '$esc_content', '$esc_language', $tot_size, '${result['status']}', '$result_json', {$problem['is_hidden']})");
+    // }
 }
 function handleCustomTestUpload($zip_file_name, $content, $tot_size)
 {
