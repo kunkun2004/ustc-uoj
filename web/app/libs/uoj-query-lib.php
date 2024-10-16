0f53b71a60040f4ffaee2a83285c40ae4e2699a5
zhao zekun <zekun@mail.ustc.edu.cn>
2024-10-16 12:34:46 +0800

    1

--- web/app/libs/uoj-query-lib.php
+++ web/app/libs/uoj-query-lib.php
@@ -131,9 +131,9 @@ function queryContestUserProblemList($contest, $user) {
 			$sql .= " and pt.tag = '".DB::escape("难度:".$pf["problem_difficulty"])."'";
 		}
 		$sql .= " group by p.id";
-		//echo $sql;
+		echo $sql;
 		$problem_list = DB::selectALL($sql);
-		var_dump($problem_list);
+		//var_dump($problem_list);
 		if (count($problem_list) <= intval($pf["problem_count"])) {
 			$problem_list_res[] = $problem_list;
 		}
