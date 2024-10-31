cb33eb938a110e6f5be1f5848116cd9ea4380d8b
zhao zekun <zekun@mail.ustc.edu.cn>
2024-10-31 22:59:19 +0800

    fix

--- web/app/pages/examNotice.php
+++ web/app/pages/examNotice.php
@@ -113,10 +113,10 @@
                 url: 'execute_php', // 服务器端的 PHP 文件
                 type: 'POST',
                 data: { 
-                    action: "update contests_registrants set has_participated = 1, finish_time = '" + date('Y-m-d H:i:s', strtotime('+<?=$lastmin?> minutes')) + "' where contest_id=" + $_GET['contest_id'] + " and username = '" + $nowUser + "'" 
+                    action: "update contests_registrants set has_participated = 1, finish_time = '" + <?=date('Y-m-d H:i:s', strtotime('+$lastmin minutes'))?> + "' where contest_id=" + $_GET['contest_id'] + " and username = '" + $nowUser + "'" 
                 }, // 传递给 PHP 的数据
                 success: function(response) {
-                    alert( "update contests_registrants set has_participated = 1, finish_time = '"+ date('Y-m-d H:i:s', strtotime('+<?=$lastmin?> minutes')) + "' where contest_id=" + $_GET['contest_id'] + " and username = '" + $nowUser + "'" );
+                    alert( "update contests_registrants set has_participated = 1, finish_time = '"+ <?=date('Y-m-d H:i:s', strtotime('+$lastmin minutes'))?> + "' where contest_id=" + $_GET['contest_id'] + " and username = '" + $nowUser + "'" );
                     location.href = "/contest/<?= $contest["id"]; ?>/problem/<?= $p["id"]; ?>";
                 },
                 error: function(xhr, status, error) {
