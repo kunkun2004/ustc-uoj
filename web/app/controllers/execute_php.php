9ec8d1ebe6c6abfe54e4701fd05d5893f11a2315
zhao zekun <zekun@mail.ustc.edu.cn>
2024-10-31 23:32:27 +0800

    1

--- web/app/controllers/execute_php.php
+++ web/app/controllers/execute_php.php
@@ -1,4 +1,6 @@
 <?php
+
+    echo "123123";
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     
     echo $_POST['act1'].date('Y-m-d H:i:s', strtotime('+'.$_POST['lastmin'].' minutes')).$_POST['act2'];
