6dd52105592c2d994a62379c73ff3bc058634e80
zhao zekun <zekun@mail.ustc.edu.cn>
2024-10-31 23:31:41 +0800

    1

--- web/app/controllers/execute_php.php
+++ web/app/controllers/execute_php.php
@@ -1,5 +1,5 @@
 <?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['act1'])) {
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     
     echo $_POST['act1'].date('Y-m-d H:i:s', strtotime('+'.$_POST['lastmin'].' minutes')).$_POST['act2'];
     DB::update($_POST['act1'].date('Y-m-d H:i:s', strtotime('+'.$_POST['lastmin'].' minutes')).$_POST['act2']);
