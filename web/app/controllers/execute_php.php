82c4a72d1c68bd2dc176437f548272950a99372b
zhao zekun <zekun@mail.ustc.edu.cn>
2024-10-31 23:52:09 +0800

    1

--- web/app/controllers/execute_php.php
+++ web/app/controllers/execute_php.php
@@ -5,8 +5,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     
     echo $_POST['act1'].date('Y-m-d H:i:s', strtotime('+'.$_POST['lastmin'].' minutes')).$_POST['act2'];
     $cid = $_GET['id'];
-    $endtime = DB::selectFirst("select * from contests where id = $cid");
-    $endtime = $endtime['end_time'];
+    $row = DB::selectFirst("select * from contests where id = $cid");
+    $endtime = new DateTime($row['end_time']);
     if($endtime > strtotime('+'.$_POST['lastmin'].' minutes'))
     {
         $endtime = strtotime('+'.$_POST['lastmin'].' minutes');
