c909b3a6c7183d32f9f6edeb9a0639aee62ca0da
zhao zekun <zekun@mail.ustc.edu.cn>
2024-11-01 10:18:38 +0800

    1

--- web/app/pages/contest_problem.php
+++ web/app/pages/contest_problem.php
@@ -21,10 +21,7 @@ ini_set("display_errors", "On");
 				else {
 					$nowUser = $myUser['username'];
 					$endtimestr = DB::selectFirst("select * from contests_registrants where contest_id={$_GET['contest_id']} and username='$nowUser'");
-					if($endtimestr['has_participated']==0)
-					{
-						redirectTo("contest/$_GET['contest_id']/register");
-					}
+
 				}
 	
 			}
