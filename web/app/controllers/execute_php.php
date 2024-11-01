6ca6fa2b8ca57be334bdd875f911a32c65daf731
zhao zekun <zekun@mail.ustc.edu.cn>
2024-11-01 09:29:58 +0800

    1

--- web/app/controllers/execute_php.php
+++ web/app/controllers/execute_php.php
@@ -1,18 +1,18 @@
 <?php
 
-    echo "123123";
+    // echo "123123";
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     
-    echo $_POST['act1'].date('Y-m-d H:i:s', strtotime('+'.$_POST['lastmin'].' minutes')).$_POST['act2'];
+    // echo $_POST['act1'].date('Y-m-d H:i:s', strtotime('+'.$_POST['lastmin'].' minutes')).$_POST['act2'];
     $cid = $_GET['id'];
     $row = DB::selectFirst("select * from contests where id = $cid");
     $endtime = new DateTime($row['end_time']);
-    if($endtime < strtotime('+'.$_POST['lastmin'].' minutes'))
+    if($endtime > strtotime('+'.$_POST['lastmin'].' minutes'))
     {
         $endtime = strtotime('+'.$_POST['lastmin'].' minutes');
     }
     DB::update($_POST['act1'].date('Y-m-d H:i:s', $endtime).$_POST['act2']);
 
-    echo $_POST['act1'].date('Y-m-d H:i:s', strtotime('+'.$_POST['lastmin'].' minutes')).$_POST['act2'];
+    echo $_POST['act1'].date('Y-m-d H:i:s', $endtime).$_POST['act2'];
 }
 ?>
