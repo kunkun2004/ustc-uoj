203b1213e8a9e10ed0de75948261385eb0ceaa26
zhao zekun <zekun@mail.ustc.edu.cn>
2024-11-01 10:45:45 +0800

    11

--- web/app/pages/contest_problem.php
+++ web/app/pages/contest_problem.php
@@ -12,18 +12,14 @@ ini_set("display_errors", "On");
 
 	if ($contest != null) {
 		if (!hasContestPermission($myUser, $contest)) {
-			if ($contest['cur_progress'] == CONTEST_NOT_STARTED) {
-				become403Page();
-			} elseif ($contest['cur_progress'] == CONTEST_IN_PROGRESS) {
-				if ($myUser == null || !hasRegistered($myUser, $contest)) {
-					becomeMsgPage("<h1>比赛正在进行中</h1><p>很遗憾，您尚未报名。比赛结束后再来看吧 ～</p>");
-				} 
-				$nowUser = $myUser['username'];
-				$endtimestr = DB::selectFirst("select * from contests_registrants where contest_id={$_GET['contest_id']} and username='$nowUser'");
-				var_dump($endtimestr);
-				echo "11111";
-	
-			}
+			if ($myUser == null || !hasRegistered($myUser, $contest)) {
+				becomeMsgPage("<h1>比赛正在进行中</h1><p>很遗憾，您尚未报名。比赛结束后再来看吧 ～</p>");
+			} 
+			$nowUser = $myUser['username'];
+			$endtimestr = DB::selectFirst("select * from contests_registrants where contest_id={$_GET['contest_id']} and username='$nowUser'");
+			var_dump($endtimestr);
+			echo "11111";
+
 		}
 	}
 
