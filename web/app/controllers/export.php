0edd3824b0aca4e7a1d2823e8b9449f03839a14c
zhao zekun <zekun@mail.ustc.edu.cn>
2024-11-04 13:13:34 +0800

    1

--- web/app/controllers/export.php
+++ web/app/controllers/export.php
@@ -16,72 +16,72 @@ $sheet = $objPHPExcel->getActiveSheet();
 
 $contest = queryContest($_GET['id']);
 
-		$problem_filters = DB::selectALL("select * from contests_problem_filters where contest_id = {$contest['id']}");
-		$contest_data = queryContestData($contest);
-		$all_registrants = DB::selectALL("select username from contests_registrants where contest_id = {$contest['id']}");
-		//var_dump($all_registrants);
-		// 统计每个用户的每个题目的分数
-		$score_list = array();
-        $sid_list = array();
-		foreach ($all_registrants as $r) {
-			$u = $r["username"];
-			$score_list[$u] = array();
-            $sid_list[$u] = array();
-				$pl = queryContestUserProblemList($contest, array("username" => $u));
-				for ($i = 0; $i < count($problem_filters); ++$i) {
-					$pf = $problem_filters[$i];
-					$ps = $pl[$i];
-					$sql = "
-    SELECT
-        p.id AS problem_id,
-        s.id AS submission_id,
-        s.score * {$pf["problem_score"]} / 100 AS pscore
-    FROM
-        problems p
-    LEFT JOIN
-        submissions s
-    ON
-        s.id = (
-            SELECT id
-            FROM submissions
-            WHERE contest_id = {$contest['id']}
-              AND submitter = '$u'
-              AND problem_id = p.id
-            ORDER BY id DESC
-            LIMIT 1
-        )
-    WHERE
-        p.id IN (" . implode(',', array_column($ps, 'id')) . ")
+$problem_filters = DB::selectALL("select * from contests_problem_filters where contest_id = {$contest['id']}");
+$contest_data = queryContestData($contest);
+$all_registrants = DB::selectALL("select username from contests_registrants where contest_id = {$contest['id']}");
+//var_dump($all_registrants);
+// 统计每个用户的每个题目的分数
+$score_list = array();
+$sid_list = array();
+foreach ($all_registrants as $r) {
+    $u = $r["username"];
+    $score_list[$u] = array();
+    $sid_list[$u] = array();
+    $pl = queryContestUserProblemList($contest, array("username" => $u));
+    for ($i = 0; $i < count($problem_filters); ++$i) {
+        $pf = $problem_filters[$i];
+        $ps = $pl[$i];
+        $sql = "
+SELECT
+p.id AS problem_id,
+s.id AS submission_id,
+s.score * {$pf["problem_score"]} / 100 AS pscore
+FROM
+problems p
+LEFT JOIN
+submissions s
+ON
+s.id = (
+    SELECT id
+    FROM submissions
+    WHERE contest_id = {$contest['id']}
+        AND submitter = '$u'
+        AND problem_id = p.id
+    ORDER BY id DESC
+    LIMIT 1
+)
+WHERE
+p.id IN (" . implode(',', array_column($ps, 'id')) . ")
 ";
-					$res = DB::selectALL($sql);
-					$score = array_column($res, 'pscore');
-					$sid = array_column($res, 'submission_id');
-					$score_list[$u][] = $score;
-                    $sid_list[$u][] = $sid;
-				}
-			}
-		//var_dump($score_list);
+        $res = DB::selectALL($sql);
+        $score = array_column($res, 'pscore');
+        $sid = array_column($res, 'submission_id');
+        $score_list[$u][] = $score;
+        $sid_list[$u][] = $sid;
+    }
+}
+//var_dump($score_list);
 
 
-		//var_dump($contest_data);
-		//calcStandings($contest, $contest_data, $score, $standings);
-		
-		// uojIncludeView('contest-standings', [
-		// 	'contest' => $contest,
-		// 	'isSuper' => isSuperUser($myUser),
-		// 	'problem_filters' => $problem_filters,
-		// 	'score_list' => $score_list,
-		// 	'contest_data' => $contest_data
-		// ]);
-        echo '<pre>';
-        print_r($score_list);
-        echo '</pre>';
-        echo '<pre>';
-        print_r($contest_data);
-        echo '</pre>';
-        echo '<pre>';
-        print_r($problem_filters);
-        echo '</pre>';
+//var_dump($contest_data);
+//calcStandings($contest, $contest_data, $score, $standings);
+
+// uojIncludeView('contest-standings', [
+// 	'contest' => $contest,
+// 	'isSuper' => isSuperUser($myUser),
+// 	'problem_filters' => $problem_filters,
+// 	'score_list' => $score_list,
+// 	'contest_data' => $contest_data
+// ]);
+// echo '<pre>';
+// print_r($score_list);
+// echo '</pre>';
+// echo '<pre>';
+// print_r($contest_data);
+// echo '</pre>';
+// echo '<pre>';
+// print_r($problem_filters);
+// echo '</pre>';
 
 
 
